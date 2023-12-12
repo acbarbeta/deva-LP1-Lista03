a34800ed36299c0471c55768f2c41965e39e0346
Ana Clara <aclara.bvicente@gmail.com>
2023-12-12 20:22:26 -0300

    exercício 05 - final

--- src/Exercicio05.java
+++ src/Exercicio05.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Exercicio05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Qual o tempo gasto na viagem, em horas?");
+        double tempoGasto = input.nextDouble();
+        System.out.println("Qual a velocidade média durante a viagem, em km/h?");
+        double velocidadeMedia = input.nextDouble();
+
+        double distanciaPercorrida = calculaDistancia(tempoGasto, velocidadeMedia);
+        double combustivelNecessario = calculaQuantidadeDeCombustivel(distanciaPercorrida);
+
+        System.out.printf("Para a viagem, serão necessários %.3f L de combustível", combustivelNecessario);
     }
 
     public static double calculaDistancia(double tempoGasto, double velocidadeMedia) {
